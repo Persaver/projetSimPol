e89426acc6283b0912ab290b7aef7386ea2f4612
Robin <robin@a>
2016-10-28 15:41:34 +0200

    map 2, test d'une nouvelle facon de faire

--- app/crafty_robin/crafty/travail-crafty-map/map2.js
+++ app/crafty_robin/crafty/travail-crafty-map/map2.js
@@ -3,34 +3,64 @@ var CraftyMap = (function() {
     //Surface de jeu
     Crafty.init(1000, 600, document.getElementById('scene'));
     Crafty.background('url(img/damier.jpg)');
-
+    var dataEntities = [{
+            type: 'fixed',
+            name: 'house_1',
+            components: "2D, DOM, map, house1",
+            attr: {
+                x: 0,
+                y: 0,
+                w: 287,
+                h: 100
+            }
+        },  {
+            type: 'moveable',
+            name: 'museum',
+            components: "2D,DOM,map,house1, Collision,Mouse, Draggable",
+            attr: {
+                x: 470,
+                y: 0,
+                w: 400,
+                h: 400
+            }
+        }, {
+            type: 'moveable',
+            name: 'warehouse',
+            components: "2D,DOM,map,house1,solid,Collision,Mouse, Draggable",
+            attr: {
+                x: 0,
+                y: 150,
+                w: 255,
+                h: 217
+            }
+        }, {
+            type: 'moveable',
+            name: 'desk',
+            components: "2D,DOM,map,desk,solid,Collision,Mouse, Draggable",
+            attr: {
+                x: 300,
+                y: 160,
+                w: 244,
+                h: 170
+            }
+        }
+    ];
     var assetsObj = {
         "sprites": {
             "./img/buildings-icon.png": {
-                "map" : {}
-                house1: [
-                    0,
-                    0,
-                    287,
-                    100
-                ]
+                "map" : {"house1":[0, 0, 287, 100],
+                        "house2":[800, 220, 80, 100],
+                        "house3": [402, 121, 128, 76]}
             },
-            './img/buildings-icon.png': {
-                house2: [
-                    800,
-                    220,
-                    80,
-                    100
-                ]
-            }
         }
-
     };
 
     Crafty.load(assetsObj, // preload assets
         function() {
-            createSprits();
+//            Crafty.e("2D,DOM,map,house1,solid,Collision,Mouse, Draggable").attr(scaleEntity({ x: 0,y: 0,w: 287,h: 100}),5);
+//            Crafty.e("2D,DOM,map,house1,solid,Collision,Mouse, Draggable").attr({ x: 0,y: 0,w: 287,h: 100});
             createEntity();
+            console.log("a termine");
         },
         function(e) {
             console.log("toto ramirez");
@@ -61,7 +91,7 @@ var CraftyMap = (function() {
     var createEntity = function() {
         for (var entity in dataEntities) {
             var entityC = dataEntities[entity];
-            craftyEntities[entityC.name] = Crafty.e(entityC.components).attr(scaleEntity(entityC.attr, 5));
+            Crafty.e(entityC.components).attr(entityC.attr);
         }
     };
 })();
