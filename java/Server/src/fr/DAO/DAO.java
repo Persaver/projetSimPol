e6d6c1131fd8aed27c35140a2f2c9e9c36ed5378
Saverio Perri <saverio.perri@mail.com>
2016-11-08 16:33:44 +0100

    modif serveur 2

--- java/Server/src/fr/DAO/DAO.java
+++ java/Server/src/fr/DAO/DAO.java
@@ -4,11 +4,8 @@ public abstract class DAO<T>{
 //	private AccessDB abd = null;
 //	private Connexion cnx = null;
 	
-	
-	public abstract T get(int id);;
-	
+	public abstract T get(int id);
 	public abstract void save(T element);
 	public abstract void delete(int id);
-	
 	public abstract void update(T element);
 }
