d319a8a42427a2eacb685885662f33bbfcdeb7e1
NaokoD <ludovic.damprobe@gmail.com>
2016-11-09 10:24:04 +0100

    Ajout BackupConstruction

---
+++ java/Server/src/fr/DAO/BackupConstructionDAO.java
@@ -0,0 +1,32 @@
+package fr.DAO;
+
+import fr.entities.BackupConstruction;
+
+public class BackupConstructionDAO extends DAO<BackupConstruction> {
+
+	@Override
+	public BackupConstruction get(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+	@Override
+	public void delete(int id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void save(BackupConstruction element) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update(BackupConstruction element) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
--- java/Server/src/fr/entities/Construction.java
+++ java/Server/src/fr/entities/Construction.java
@@ -3,5 +3,77 @@ package fr.entities;
 import java.io.Serializable;
 
 public class Construction implements Serializable{
+	private int id;
+	private String designation;
+	private int h;
+	private int w;
+	
+	private Categorie categorie;
 
+	public Construction(int id, String designation, int h, int w, Categorie categorie) {
+		super();
+		this.id = id;
+		this.designation = designation;
+		this.h = h;
+		this.w = w;
+		this.categorie = categorie;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getDesignation() {
+		return designation;
+	}
+
+	public void setDesignation(String designation) {
+		this.designation = designation;
+	}
+
+	public int getH() {
+		return h;
+	}
+
+	public void setH(int h) {
+		this.h = h;
+	}
+
+	public int getW() {
+		return w;
+	}
+
+	public void setW(int w) {
+		this.w = w;
+	}
+
+	public Categorie getCategorie() {
+		return categorie;
+	}
+
+	public void setCategorie(Categorie categorie) {
+		this.categorie = categorie;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("Construction [id=");
+		builder.append(id);
+		builder.append(", designation=");
+		builder.append(designation);
+		builder.append(", h=");
+		builder.append(h);
+		builder.append(", w=");
+		builder.append(w);
+		builder.append(", categorie=");
+		builder.append(categorie);
+		builder.append("]");
+		return builder.toString();
+	}
+	
 }
