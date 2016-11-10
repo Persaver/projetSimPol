5d5ca530c49afe7b52b7e240cda4a50e900944a6
Llemilian <robin-andco@hotmail.fr>
2016-11-10 16:46:34 +0100

    voila le debut

--- java/BaseSimPol V1/src/fr/batiment/Banque.java
+++ java/BaseSimPol V1/src/fr/batiment/Banque.java
@@ -1,38 +1,23 @@
 package fr.batiment;
 
-import fr.indicateur.Budget;
-
 public class Banque extends Batiment {
 
 	public Banque() {
 		this.nbSalarie = 120;
-		Budget.setNbSalaries(12);
 		this.nbCadre = 30;
-		Budget.setNbCadre(3);
+		this.modRisque = 3;
 	}
 
 	public Banque(int niv) {
 		this.nbSalarie = 70+60*(niv-1);
-		Budget.setNbSalaries(this.nbSalarie/10);
 		this.nbCadre = (int)(30*Math.pow(1.3, niv-1));
-		Budget.setNbCadre(this.nbCadre/10);
+		this.modRisque = 3*niv;
 	}
 	
 	public void Ameliore (){
-		int newSalarie = 60;
-		int newCadre = (int)(this.nbCadre*0.3);
-		if ((((this.nbCadre + newCadre)/10>(this.nbCadre)/10))){				// Pour garder une coherence dans les chiffres
-			Budget.setNbCadre((this.nbCadre+newCadre)/10-this.nbCadre/10);		// Les nouveaux postes de cares sont directement renseigner
-		}
-		this.nbCadre += newCadre;
-		if ((this.nbSalarie + newSalarie)/10>(this.nbSalarie)/10){
-			Budget.setNbSalaries((this.nbSalarie+newSalarie)/10-this.nbSalarie/10);
-		}
-		this.nbSalarie += newSalarie;
-	}
-
-	public void usure (){		// Regulierement, le batiment s'use
-		this.risque +=3;
+		this.nbSalarie += 60;
+		this.nbCadre += (int)(this.nbCadre*0.3);
+		this.modRisque += 3;
 	}
 	
 }
--- java/BaseSimPol V1/src/fr/batiment/Batiment.java
+++ java/BaseSimPol V1/src/fr/batiment/Batiment.java
@@ -4,10 +4,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 import fr.indicateur.Budget;
+import fr.indicateur.Population;
 
 public abstract class Batiment {
 	protected int nbCadre;				// Pour leur permettre d'evoluer, les nombres de poste seront multiplies par 10
 	protected int nbSalarie;
+	protected int modRisque;
 	protected int risque = 0;
 	protected static List<Batiment>construction = new ArrayList<Batiment>(); 
 	
@@ -17,37 +19,77 @@ public abstract class Batiment {
 	
 	public Batiment (int niv){
 	}
-	
-	public int coutPers(){
-		int p = this.nbCadre*Budget.getSalaireCadre()+
-				this.nbSalarie*Budget.getSalaireStandard()/*+
-				this.nbHautDiplome*Budget.getSalaireHautDiplome()+
-				this.nbSpecialiste*Budget.getSalaireSpecialiste()*/;
-		return p;	
-	}
+
 	
 	public void detruire(){
 	}
 	public void ameliorer(){
 	}
+
+	
+		// Fonction individuelles
 	public int getNbCadre() {
 		return nbCadre/10;
 	}
 	public int getNbSalarie() {
 		return nbSalarie/10;
 	}
+	public int getRisque(){
+		return this.risque;
+	}
+	public void modRisque(int mod){
+		this.risque -= mod;
+	}
+	public int coutPers(){
+		int p = this.nbCadre*Budget.getSalaireCadre()+
+				this.nbSalarie*Budget.getSalaireStandard();
+		return p;
+	}	
+	public void usure (){			// Regulierement, le batiment s'use
+		this.risque += this.modRisque;
+	}
 	
+		// Fonctions Communes
 	public static void afficheList(){
 		for (int i = 0; i < construction.size(); i++){
 			System.out.println(construction.get(i));
 		}
 	}
-	
 	public static int getNbSalaries(){
 		int nb = 0;
 		for (int i = 0; i < construction.size(); i++){
 			nb += construction.get(i).getNbSalarie();
 		}
 		return nb;
+	}
+	public static int getNbCadres(){
+		int nb = 0;
+		for (int i = 0; i < construction.size(); i++){
+			nb += construction.get(i).getNbCadre();
+		}
+		return nb;
+	}
+	public static int getTotalRisque(){
+		int r = 0;
+		for (int i = 0; i < construction.size(); i++){
+			r += construction.get(i).getRisque();
+		}
+		return r;
+	}
+	public static void usures(){	// fonction qui fait s'user tous les batiments chaque jour
+		for (int i = 0; i < construction.size(); i++){
+			construction.get(i).usure();
+		}
+	}
+	public static void afficheEtat(){
+		for (int i = 0; i < construction.size(); i++){
+			System.out.println("Batiment de type " +construction.get(i).getClass() + " avec un risque d'accident de " + construction.get(i).getRisque() + "%");
+		}
+	}
+	public static int necessiteux (Population p){		// par jour?
+		int accident = (getTotalRisque()*p.nbIndiv(3, Budget.getAgeRetraite())/100)/construction.size();
+		int malades = p.nbIndiv(0, 129)/* *(100-Hygiene)*/ /85;
+		int necessiteux = accident + malades;
+		return necessiteux;
 	}
 }
